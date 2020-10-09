984285673196f2ea89c28470759eda573ae70f06
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-09 11:22:57 -0600

    Visualization code added

--- Hierarchical_Clustering.py
+++ Hierarchical_Clustering.py
@@ -17,3 +17,14 @@ X = dataset.iloc[:, [3,4]].values
 
 
 # Visualize the clusters
+plt.legend()
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'green', label = 'Cluster 2')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'blue', label = 'Cluster 3')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'purple', label = 'Cluster 4')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'yellow', label = 'Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'black', label = "Centroids")
+plt.title('Clusters of Customers')
+plt.xlabel('Annual Income (K$)')
+plt.ylabel('Spending Score (1-100)')
+plt.show();
