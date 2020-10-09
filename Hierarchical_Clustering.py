c6f6437b3ab2a1d78bb243ae3193f1a43a19252e
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-09 11:20:33 -0600

    Set up template in Spyder

--- Hierarchical_Clustering.py
+++ Hierarchical_Clustering.py
@@ -9,3 +9,11 @@ import pandas as pd
 # Import the dataset
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3,4]].values
+
+# Use the Dendrogram to find the optimal number of clusters
+
+
+# Train the Hierarchical CLustering model on the dataset
+
+
+# Visualize the clusters
