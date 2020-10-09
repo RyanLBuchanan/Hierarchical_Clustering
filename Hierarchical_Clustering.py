03e0680571e229308d29b36af630fcf9628e702d
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-09 11:18:25 -0600

    Import libraries

--- Hierarchical_Clustering.py
+++ Hierarchical_Clustering.py
@@ -1,2 +1,11 @@
 # Hierarchical Clustering ALgorithm tutorial from Machine Learning A-Z - SuperDataScience
 # Input by Ryan L Buchanan 08OCT20
+
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3,4]].values
